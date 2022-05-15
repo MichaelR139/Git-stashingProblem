9eef693c04396eeca9b3beb422119fa0f4f1b588
MichaelR139 <michaelradvinsky@gmail.com>
2022-05-15 11:19:16 -0700

    pp

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("pppppppppppppppppp");
+        int a=9;
+        int b=10;
+        int c=a+b;
+        System.out.println(c);
+    }
+}

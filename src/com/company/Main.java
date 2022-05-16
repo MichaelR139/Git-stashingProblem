680d37e39cf8ba0bc4c2a935cc84e72e95a52069
MichaelR139 <michaelradvinsky@gmail.com>
2022-05-16 13:29:04 -0700

    3th

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         System.out.println("pppppppppppppppppp");
-        int a=9;
-        int b=10;
+        int a=12;
+        int b=11;
         int c=a+b;
         System.out.println(c);
         System.out.println(c);
